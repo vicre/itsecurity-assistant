351e30a4798a658507e7c2bfce5c84a62bf8e754
Victor Reipur <vicre@dtu.dk>
2024-11-26 13:30:29 +0000

    next.config

--- app-main/next.config.js
+++ app-main/next.config.js
@@ -1,14 +1,13 @@
 const isLocal = process.env.NODE_ENV === 'development';
 
-module.exports = {
+export default {
   images: {
-    domains: [],
+    domains: ['firebasestorage.googleapis.com'],
   },
   webpack(config, { dev }) {
     if (dev) {
       config.devtool = 'cheap-module-source-map';
     }
     return config;
-  },
-  // Remove the 'functions' key as it's not valid in Next.js configuration
-};
+  }
+}
